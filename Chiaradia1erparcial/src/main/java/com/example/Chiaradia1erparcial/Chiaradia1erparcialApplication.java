2a15f1391d3f95504f40617cc7b1b42584fb47b4
ignacio chiaradia <ignachy01@gmail.com>
2021-04-14 17:40:02 -0300

    first commit

---
+++ Chiaradia1erparcial/src/main/java/com/example/Chiaradia1erparcial/Chiaradia1erparcialApplication.java
@@ -0,0 +1,13 @@
+package com.example.Chiaradia1erparcial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chiaradia1erparcialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chiaradia1erparcialApplication.class, args);
+	}
+
+}
---
+++ Chiaradia1erparcial/src/test/java/com/example/Chiaradia1erparcial/Chiaradia1erparcialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Chiaradia1erparcial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chiaradia1erparcialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
